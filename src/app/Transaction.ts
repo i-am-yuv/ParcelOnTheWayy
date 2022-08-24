93f6b0cb416e586b45e231999a1a8dfbd96981f7
Saini-Shaab <atul.saini@peoplestrong.com>
2022-08-24 20:08:03 +0530

    showActiveTravellersAdded

---
+++ src/app/Transaction.ts
@@ -0,0 +1,13 @@
+export class Transaction{
+    transactionId!: number;
+    transactionDate!: Date;
+    travellerId!: number;
+    vehicleId!: number;
+    startTime!: number;
+    endTime!: number;
+    travellerStatus!:String;
+    startLocation!: String;
+    endLocation! : String;
+    deliverDate!:Date;
+    
+}
--- src/app/user/user/user.component.ts
+++ src/app/user/user/user.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Transaction } from 'src/app/Transaction';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-user',
@@ -6,10 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
+  private apiUrl = environment.apiBasedUrl ;
 
   btnVisible : Boolean = true ; 
   
-  constructor() { }
+  //transaction:Transaction=new Transaction();
+
+  public transactions:Transaction[]=[];
+
+  constructor(private http : HttpClient) { }
 
   ngOnInit(): void 
   {
@@ -22,6 +30,23 @@ export class UserComponent implements OnInit {
     {
       this.btnVisible = true ;
     }
+    this.getActiveTravellers();
   }
 
-}
+
+getActiveTravellers()
+   {
+    this.http.get<Transaction[]>(  `${this.apiUrl}/getActiveTravellers` ).subscribe(
+      res => 
+      {
+       // console.log(res);
+        this.transactions=res;
+        console.log(this.transactions);
+      }
+      , err => {
+        // this.toast.error({detail:"ERROR",summary:'Add User Info',duration:5000});
+        console.log(err) ;
+      }
+    )
+   }
+  }
