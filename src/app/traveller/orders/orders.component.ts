75b7ac49e8613a38a44abe44e9e2236f8a739bc8
varun-0108 <111352700+varun-0108@users.noreply.github.com>
2022-08-25 07:21:43 +0530

    order details U

--- src/app/traveller/orders/orders.component.ts
+++ src/app/traveller/orders/orders.component.ts
@@ -1,4 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NgToastService } from 'ng-angular-popup';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-orders',
@@ -6,10 +11,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./orders.component.css']
 })
 export class OrdersComponent implements OnInit {
+  
+  private apiUrl = environment.apiBasedUrl ;
+  public orderSendValues !: FormGroup ;
 
-  constructor() { }
+    
+  constructor( private formBuilder : FormBuilder , private http : HttpClient , private router : Router , private toast: NgToastService )
+   { }
 
   ngOnInit(): void {
+    this.orderSendValues = this.formBuilder.group(
+      {
+        //  transactionId:['',Validators.required] ,
+        // travellerId:['' , Validators.required],
+        userId:['' , Validators.required],
+        orderStatus:['' , Validators.required],
+        // modeOfTravel:['' , Validators.required],
+        dropLocation:['' , Validators.required],
+        pickUpLocation:['' , Validators.required],
+        expectedStartTime:['' , Validators.required],
+        expectedEndTime:['' , Validators.required]
+      }
+    )
   }
-
+  
+  orderSend(){
+    this.http.post<any>( `${this.apiUrl}/orderDetails/add`  , this.orderSendValues.value )
+       .subscribe(
+        res=>{
+          this.toast.success({detail:"SUCCESS",summary:"Order Sent Successfully" ,duration:5000});
+          this.orderSendValues.reset() ;
+        },
+        err=>{
+          this.toast.warning({detail:"WARN",summary:"error in data",duration:5000}); 
+        }
+       )
+  }
 }
