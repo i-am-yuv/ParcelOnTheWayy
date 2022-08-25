9fa0a8631d55f3c81dff76e5b084eb5d0497b125
Yuvraj Bhati <yuvrajbhati2507@gmail.com>
2022-08-25 10:59:39 +0530

    traveller table is created

--- src/app/traveller/add-traveller/add-traveller.component.ts
+++ src/app/traveller/add-traveller/add-traveller.component.ts
@@ -1,8 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { NgToastService } from 'ng-angular-popup';
+import { Traveller } from 'src/app/Traveller';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -13,17 +14,40 @@ import { environment } from 'src/environments/environment';
 export class AddTravellerComponent implements OnInit 
 {
   private apiUrl = environment.apiBasedUrl;
-  public aadharValues !: FormGroup;
+  public formValues !: FormGroup;
 
 
   constructor(private formBuilder: FormBuilder, private http: HttpClient, private router: Router, private toast: NgToastService ) { }
 
   ngOnInit(): void {
+    this.formValues = this.formBuilder.group(
+      {
+        aadharno: ['', Validators.required] 
+      })
   }
 
-  addAadhar()
-  {
+  travellerObj : Traveller = new Traveller() ;
 
+  addTraveller()
+  {
+    this.travellerObj.id = Number( sessionStorage.getItem('loginId') ) ;
+    this.travellerObj.aadharno = this.formValues.value.aadharno ;
+
+    this.http.post<any>(`${this.apiUrl}/traveller/add` , this.travellerObj )
+    .subscribe(
+      res => 
+      {
+        console.log(res) ;
+          this.toast.success({detail:"SUCCESS",summary:'Aadhar Number Added Successfully',duration:2000});
+          this.formValues.reset();
+          sessionStorage.setItem('travllerId' , res.travellerId) ;
+      }
+      , err => {
+        console.log(err);
+        this.formValues.reset();
+        this.toast.error({detail:"ERROR",summary:'Aadhar Number is Already Added',duration:5000});
+      }
+    )
   }
 
 }
